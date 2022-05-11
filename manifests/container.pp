10e2e977e292773ea99a0587cd0dfed83ce0ab7e
Les Aker <me@lesaker.org>
2022-05-10 21:31:57 -0400

    add templates

--- manifests/container.pp
+++ manifests/container.pp
@@ -1,9 +1,23 @@
 # @summary Create a Docker container
 #
 # @param image sets the Docker image to fetch for the container
+# @param args sets the arguments for the docker run command
+# @param cmd sets the command to launch the container with
 # @param container_name (namevar) sets the name of the container
-define smb::mount (
+define docker::container (
   String $image,
+  String $args,
+  String $cmd,
   String $container_name = $title,
 ) {
+  file { "/etc/container/${container_name}":
+    ensure  => file,
+    content => template('docker/container.erb'),
+  }
+
+  ~> service { "container@${container_name}":
+    ensure  => running,
+    enable  => true,
+    require => File['/etc/systemd/system/container@.service'],
+  }
 }
--- manifests/init.pp
+++ manifests/init.pp
@@ -6,6 +6,16 @@ class docker (
 ) {
   package { 'docker': }
 
+  -> service { 'docker':
+    ensure => running,
+    enable => true,
+  }
+
+  file { '/etc/systemd/system/container@.service':
+    ensure => file,
+    source => 'puppet:///modules/docker/container@.service',
+  }
+
   $docker::containers.each | String $name, Hash $options | {
     docker::container { $name:
       * => $options,
